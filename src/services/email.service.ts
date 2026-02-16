3446d64f7755355853809f494a37e05d404e9fa7
Annas Putra Anuraga <annasputra52@gmail.com>
2026-02-16 16:52:53 +0700

    asdw

--- src/services/email.service.ts
+++ src/services/email.service.ts
@@ -5,7 +5,7 @@ dotenv.config();
 
 const transporter = nodemailer.createTransport({
     host: process.env.SMTP_HOST || 'smtp.gmail.com',
-    port: Number(process.env.SMTP_PORT) || 587,
+    port: Number(process.env.SMTP_PORT) || 465,
     secure: process.env.SMTP_SECURE === 'true', // true for 465, false for other ports
     auth: {
         user: process.env.SMTP_USER,
